1a1c60f422e964e08303846aa0293a08d67ec445
Andrew Chen <andrew.chen7@student.tdsb.on.ca>
2024-07-15 17:37:49 -0400

    tweak

--- client/src/components/NavBar/translate.js
+++ client/src/components/NavBar/translate.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 //google translate button script 
 
+//translate function component
 const Translate = () => {
+
+  //sets the initialization to create a new translate widget 
   const googleTranslateElementInit = () => {
     new window.google.translate.TranslateElement(
       {
@@ -12,19 +15,22 @@ const Translate = () => {
     );
   };
 
+
   useEffect(() => {
     window.googleTranslateElementInit = googleTranslateElementInit;
 
+    //adds onto the script
     const addScript = document.createElement("script");
+    console.log("loaded");
     addScript.setAttribute(
       "src",
       "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
     );
     document.body.appendChild(addScript);
-
     return () => {
       document.body.removeChild(addScript);
     };
+
   }, []);
 
   return (
