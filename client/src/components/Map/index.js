2697d5106e06b3a7d5320235c133a049e4c1b572
kaiyanchu <kchu@bayviewglen.ca>
2025-06-23 18:04:27 -0400

    map

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -5,7 +5,8 @@ class Map extends Component {
   render() {
     // Access the site prop and construct the image path dynamically
     const { site } = this.props; // Get the site prop
-    const mapImagePath = `../../../images/${site}-Maps/general.png`; // Construct the path based on site
+    const capitalizedSite = site.charAt(0).toUpperCase() + site.slice(1); // Capitalize first letter
+    const mapImagePath = `../../../images/${capitalizedSite}-Maps/general.png`; // Use capitalized site
 
     return (
       <>
