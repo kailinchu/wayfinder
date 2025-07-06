240bd37f77e6a9569d19d5bb49180c555a154fa6
slxng1758 <148385226+slxng1758@users.noreply.github.com>
2025-07-06 23:23:41 +0800

    added env file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjixRIUPTurJHfHNnV-sxoDJwob1wSZPk",
-  authDomain: "shn-wayfinder-4dbf1.firebaseapp.com",
-  projectId: "shn-wayfinder",
-  storageBucket: "shn-wayfinder-4dbf1.firebasestorage.app",
-  messagingSenderId: "272926547066",
-  appId: "1:272926547066:web:1d49202154fc8203af6fc3",
-  measurementId: "G-W5W7R2VXB4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
